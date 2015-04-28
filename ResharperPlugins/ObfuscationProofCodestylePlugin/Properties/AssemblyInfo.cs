949a29efb765c9e421246f20e152cd997fcd69de
Nikolay Hidalgo Diaz <n.idalgo@abbyy-ls.com>
2015-04-28 23:36:06 +0300

    renaming

--- ResharperPlugins/ObfuscationProofCodestylePlugin/Properties/AssemblyInfo.cs
+++ ResharperPlugins/ObfuscationProofCodestylePlugin/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using JetBrains.Application.PluginSupport;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OfuscationCodestylePlugin")]
+[assembly: AssemblyTitle("OfuscationProofCodestylePlugin")]
 [assembly: AssemblyDescription("Inspects code to make sure it follows obfuscation-proof patterns. Provides quick fixes to found errors.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Abbyy Language Services")]
-[assembly: AssemblyProduct("Obfuscation codestyle ReSharper plugin")]
+[assembly: AssemblyProduct("Obfuscation-proof codestyle ReSharper plugin")]
 [assembly: AssemblyCopyright("Copyright © Abbyy Language Services, 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -17,9 +17,9 @@ using JetBrains.Application.PluginSupport;
 [assembly: AssemblyVersion("1.1.0.0")]
 [assembly: AssemblyFileVersion("1.1.0.0")]
 
-[assembly: ActionsXml("OfuscationCodestylePlugin.Actions.xml")]
+[assembly: ActionsXml("OfuscationProofCodestylePlugin.Actions.xml")]
 
 // The following information is displayed by ReSharper in the Plugins dialog
-[assembly: PluginTitle("Obfuscation codestyle")]
+[assembly: PluginTitle("Obfuscation proof codestyle")]
 [assembly: PluginDescription("Inspects code to make sure it follows obfuscation-proof patterns. Provides quick fixes to found errors.")]
 [assembly: PluginVendor("Abbyy Language Services")]
